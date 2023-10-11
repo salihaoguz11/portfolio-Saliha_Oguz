c7e643ec72257b9e299f23b63194cb1e2535253f
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-11 09:36:49 +0100

    set eventlistener to the links

--- .vscode/app.js
+++ .vscode/app.js
@@ -1,1 +1,18 @@
 const links=document.querySelectorAll('.link')
+const sections=document.querySelectorAll('.section')
+
+let activeLink=0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click',()=>{
+        if (active!=i) {
+            links[activeLink].classList.remove('active')
+            link.classList.add('active')
+            sections[activeLink].classList.remove('active')
+            setTimeout(() => {
+                activeLink=i
+                sections[i].classList.add('active')
+            }, 1000);
+        }
+    })
+})
