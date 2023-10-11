c5ae683c07e7a5ae9cc6b10f5a33540776eca643
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-11 09:43:35 +0100

    active link

--- .vscode/app.js
+++ .vscode/app.js
@@ -1,11 +1,11 @@
 const links=document.querySelectorAll('.link')
-const sections=document.querySelectorAll('.section')
+const sections=document.querySelectorAll('section')
 
 let activeLink=0;
 
 links.forEach((link, i) => {
     link.addEventListener('click',()=>{
-        if (active!=i) {
+        if (activeLink!=i) {
             links[activeLink].classList.remove('active')
             link.classList.add('active')
             sections[activeLink].classList.remove('active')
