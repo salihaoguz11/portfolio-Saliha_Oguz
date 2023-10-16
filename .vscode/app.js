d663fdf5d9ce6969c08c4a236c009b06946a0b3d
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-16 14:29:00 +0100

    basic css and js completed

--- .vscode/app.js
+++ .vscode/app.js
@@ -4,15 +4,22 @@ const sections=document.querySelectorAll('section')
 let activeLink=0;
 
 links.forEach((link, i) => {
-    link.addEventListener('click',()=>{
-        if (activeLink!=i) {
-            links[activeLink].classList.remove('active')
-            link.classList.add('active')
-            sections[activeLink].classList.remove('active')
-            setTimeout(() => {
-                activeLink=i
-                sections[i].classList.add('active')
-            }, 1000);
-        }
+    link.addEventListener('click',(e)=>{
+        e.preventDefault()
+        sections[i].scrollIntoView({behavior:"smooth"})
+
+        links[activeLink].classList.remove('active')
+        link.classList.add('active')
+        activeLink=i;
+
+        // if (activeLink!=i) {
+        //     links[activeLink].classList.remove('active')
+        //     link.classList.add('active')
+        //     sections[activeLink].classList.remove('active')
+        //     setTimeout(() => {
+        //         activeLink=i
+        //         sections[i].classList.add('active')
+        //     }, 1000);
+        // }
     })
 })
